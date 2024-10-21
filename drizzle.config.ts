537736b7d7298e1da364af924c9ecde0f9dec1a6
Moinul Haq <moinulhaq.se@gmail.com>
2024-10-21 08:28:29 -0400

    Fix drizzle config and add commands

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
-export default {
-  schema: "./db/schema.ts",
+export default defineConfig({
+  schema: "./src/drizzle/schema.ts",
   out: "./drizzle",
   driver: "better-sqlite",
   dbCredentials: {
-    url: "./sqlite.db"
-  }
-} satisfies Config;
+    url: "./sqlite.db",
+  },
+});
